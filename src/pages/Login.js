360d3fdbee65fb854e17a6716bbd140d3fa26dd1
danrleywillyan <danrleywillian@gmail.com>
2019-08-22 22:45:36 -0300

    all done, need match screen

---
+++ src/pages/Login.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import api from '../services/api';
+import logo from '../assets/logo.svg';
+import './Login.css';
+
+export default function Login({ history }) {
+  const [username, setUsername] = useState('');
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const response = await api.post('/devs', {
+      username: username,
+    });
+
+    const { _id } = response.data;
+    
+
+    history.push(`/devs/${_id}`);
+
+  }
+
+  return (
+    <div className="login-container">
+      <form onSubmit={handleSubmit}>
+        <img src={logo} alt="Tindev" />
+        <input
+          type="text"
+          placeholder="Usuário GitHub"
+          value={username}
+          onChange={ e => setUsername(e.target.value)}
+        />
+        <button type="submit">Enviar</button>
+      </form>
+    </div>
+  );
+}
+
+// export default Login; pode ficar aqui
